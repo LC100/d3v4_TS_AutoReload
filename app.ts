b269ac7693a34143015fc1f6a2dee09b905cae64
LC100 <lael@lael.com>
2017-03-18 09:55:02 -0400

    Made Responsive Retry

--- app.ts
+++ app.ts
@@ -8,4 +8,19 @@ message.innerHTML = "Connected to app.js";
 
 var dataArray = [5, 11, 18];
 
-d3.select("#d3Div").append("svg");
+d3.select(window).on("resize",adjustSize);
+
+function adjustSize() {
+    
+    //If needed, reset the svg and its data
+    var svgTest = d3.select("body").select("svg"); //or which ever parent tag in place of body
+    if (!svgTest.empty()){svgTest.remove();};
+
+    // Set up the width and height
+    var width = window.innerWidth||document.documentElement.clientWidth||document.body.clientWidth;
+    var height = window.innerHeight||document.documentElement.clientHeight||document.body.clientHeight;
+
+    //All the rest of your code in here
+    d3.select("#d3Div").append("svg");
+
+};
